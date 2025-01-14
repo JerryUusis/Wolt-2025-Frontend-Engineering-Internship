477c86c9c6d35391677f50f4c018a9856a8d3560
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-14 11:34:44 +0200

    Initialize UI and theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
 
 function App() {
   return (
@@ -12,8 +15,9 @@ function App() {
         width: "100vw",
         alignItems: "center",
         justifyContent: "center",
-        height:"100vh"
+        height: "100vh",
       }}
+      component={"form"}
     >
       <Box
         sx={{
@@ -24,12 +28,29 @@ function App() {
           gap: "1rem",
         }}
       >
-        <Typography variant="h1">Delivery Calculator</Typography>
-        <TextField />
-        <TextField />
-        <TextField />
+        <Typography variant="h1">Delivery Order Price Calculator</Typography>
+        <TextField label="Venue slug" />
+        <TextField label="Cart value" />
+        <TextField label="User latitude" />
+        <TextField label="User longitude" />
         <Button>Get location</Button>
         <Button>Calculate delivery fee</Button>
+        <Box sx={{ width: "100%" }}>
+          <Typography variant="h2">Price Breakdown</Typography>
+          <List>
+            <ListItem secondaryAction={<span>0e</span>}>Cart value</ListItem>
+            <ListItem secondaryAction={<span>0e</span>}>Delivery fee</ListItem>
+            <ListItem secondaryAction={<span>0e</span>}>
+              Delivery distance
+            </ListItem>
+            <ListItem secondaryAction={<span>0e</span>}>
+              Small order surcharge
+            </ListItem>
+            <ListItem secondaryAction={<span>0e</span>}>Cart value</ListItem>
+            <Divider />
+            <ListItem secondaryAction={<span>0e</span>}>Total price</ListItem>
+          </List>
+        </Box>
       </Box>
     </Box>
   );
