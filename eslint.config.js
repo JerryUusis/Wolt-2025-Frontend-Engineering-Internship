bf50aae7cd9fe1710dad534078fe16215cd2f40d
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-20 09:36:24 +0200

    Initialize and configure render and unit testing setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import vitestGlobals from "eslint-plugin-vitest-globals"; // https://github.com/saqqdy/eslint-plugin-vitest-globals?tab=readme-ov-file#usage
+import vitest from "@vitest/eslint-plugin"; // https://github.com/vitest-dev/eslint-plugin-vitest?tab=readme-ov-file#usage
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "plugin:vitest-globals/recommended",
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "vitest-globals": vitestGlobals,
+      vitest,
     },
     rules: {
+      ...vitest.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
-  },
-)
+    env: {
+      "vitest-globals/env": true,
+      browser: true,
+      jsdom: true,
+      es2020: true,
+    },
+  }
+);
