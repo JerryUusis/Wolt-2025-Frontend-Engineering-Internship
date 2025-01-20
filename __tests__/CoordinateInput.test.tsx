68588aa12d3ba8f0b60131d9b33add9bb5c5793d
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-20 11:47:40 +0200

    Add describe blocks for initial render and user interactions

--- __tests__/CoordinateInput.test.tsx
+++ __tests__/CoordinateInput.test.tsx
@@ -27,34 +27,38 @@ describe("<CoordinateInput />", () => {
     cleanup();
   });
 
-  test("should render", () => {
-    const inputField = screen.getByTestId(dataTestId);
-    expect(inputField).toBeVisible();
+  describe("initial render", () => {
+    test("should render", () => {
+      const inputField = screen.getByTestId(dataTestId);
+      expect(inputField).toBeVisible();
+    });
+    test("should have right default value", () => {
+      const inputField = screen.getByTestId(dataTestId);
+      expect(inputField).toHaveValue(value);
+    });
+    test("should have right label text", async () => {
+      const inputLabel = await screen.findByLabelText(label);
+      expect(inputLabel).toBeVisible();
+    });
   });
-  test("should have right default value", () => {
-    const inputField = screen.getByTestId(dataTestId);
-    expect(inputField).toHaveValue(value);
-  });
-  test("should have right label text", async () => {
-    const inputLabel = await screen.findByLabelText(label);
-    expect(inputLabel).toBeVisible();
-  });
-  test("should call 'setState' with zero (0) when input is empty", () => {
-    const inputField: HTMLInputElement = screen.getByTestId(dataTestId);
-    userEvent.clear(inputField);
+  describe("user interaction", () => {
+    test("should call 'setState' with zero (0) when input is empty", () => {
+      const inputField: HTMLInputElement = screen.getByTestId(dataTestId);
+      userEvent.clear(inputField);
 
-    expect(inputField.value).toBe("");
-    expect(setMockState).toHaveBeenCalledOnce();
-    expect(setMockState).toHaveBeenCalledWith(0);
-  });
-  test("should set 'setState' to user's desired value", async () => {
-    const inputField: HTMLInputElement = screen.getByTestId(dataTestId);
-    userEvent.clear(inputField);
+      expect(inputField.value).toBe("");
+      expect(setMockState).toHaveBeenCalledOnce();
+      expect(setMockState).toHaveBeenCalledWith(0);
+    });
+    test("should set 'setState' to user's desired value", async () => {
+      const inputField: HTMLInputElement = screen.getByTestId(dataTestId);
+      userEvent.clear(inputField);
 
-    const newValue = "20.54321";
-    await userEvent.type(inputField, newValue);
+      const newValue = "20.54321";
+      await userEvent.type(inputField, newValue);
 
-    expect(inputField.value).toBe(newValue);
-    expect(setMockState).toHaveBeenLastCalledWith(Number(newValue));
+      expect(inputField.value).toBe(newValue);
+      expect(setMockState).toHaveBeenLastCalledWith(Number(newValue));
+    });
   });
 });
