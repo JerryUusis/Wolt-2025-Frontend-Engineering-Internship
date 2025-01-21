99b7507487915afdf4db332e3f9eb2695a6b15d5
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-21 10:24:28 +0200

    Add test cases for distance and raw value 0

--- __tests__/SummaryListItem.test.tsx
+++ __tests__/SummaryListItem.test.tsx
@@ -1,5 +1,4 @@
 import { cleanup, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
 import SummaryListItem, {
   SummaryListItemProps,
 } from "../src/components/SummaryListItem";
@@ -8,21 +7,26 @@ import { renderWithTheme } from "./renderTestLibrary";
 import { theme } from "../src/utils/theme";
 
 describe("<SummaryListItem.tsx />", () => {
-  describe("formatting currency", () => {
+  describe("formatting meters", () => {
     const testDataValues: SummaryListItemProps = {
-      description: "currency list item",
-      rawValue: 1000,
-      formatType: "euro",
+      description: "distance list item",
+      rawValue: 750,
+      formatType: "meter",
     };
-    const { description, rawValue, formatType } = testDataValues;
+    const { description, rawValue } = testDataValues;
+
     beforeEach(() => {
       renderWithTheme(<SummaryListItem {...testDataValues} />, theme);
     });
 
     describe("initial render", () => {
-      test("should render with correct description", async () => {
+      test("should render", async () => {
         const listItem = await screen.findByRole("listitem");
         expect(listItem).toBeVisible();
+      });
+      test("should render with correct description", async () => {
+        const listItem = await screen.findByRole("listitem");
+        expect(listItem).toHaveTextContent(description);
       });
       test("should render with correct value and format", async () => {
         const listItem = await screen.findByRole("listitem");
@@ -34,6 +38,75 @@ describe("<SummaryListItem.tsx />", () => {
           ?.querySelector(".MuiListItemSecondaryAction-root span");
 
         expect(valueWrapper).toBeInTheDocument();
+        expect(valueWrapper).toBeVisible();
+        expect(valueWrapper?.textContent).toBe(
+          formatRawValue(rawValue, "meter")
+        );
+      });
+      test("should render with correct attributes", async () => {
+        const listItem = await screen.findByRole("listitem");
+        const valueWrapper = listItem
+          .closest("li")
+          ?.querySelector(".MuiListItemSecondaryAction-root span");
+
+        expect(valueWrapper).toHaveAttribute(
+          "data-raw-value",
+          rawValue.toString()
+        );
+      });
+      test("should render correctly with raw value 0", async () => {
+        cleanup();
+        const testDataValues: SummaryListItemProps = {
+          description: "distance list item",
+          rawValue: 0,
+          formatType: "meter",
+        };
+        const { rawValue } = testDataValues;
+        renderWithTheme(<SummaryListItem {...testDataValues} />, theme);
+        const listItem = await screen.findByRole("listitem");
+        const valueWrapper = listItem
+          .closest("li")
+          ?.querySelector(".MuiListItemSecondaryAction-root span");
+        expect(valueWrapper?.textContent).toBe(
+          formatRawValue(rawValue, "meter")
+        );
+        expect(valueWrapper).toHaveAttribute(
+          "data-raw-value",
+          rawValue.toString()
+        );
+      });
+    });
+  });
+
+  describe("formatting currency", () => {
+    const testDataValues: SummaryListItemProps = {
+      description: "currency list item",
+      rawValue: 1000,
+      formatType: "euro",
+    };
+    const { description, rawValue } = testDataValues;
+
+    beforeEach(() => {
+      renderWithTheme(<SummaryListItem {...testDataValues} />, theme);
+    });
+
+    describe("initial render", () => {
+      test("should render", async () => {
+        const listItem = await screen.findByRole("listitem");
+        expect(listItem).toBeVisible();
+      });
+      test("should render with correct description", async () => {
+        const listItem = await screen.findByRole("listitem");
+        expect(listItem).toHaveTextContent(description);
+      });
+      test("should render with correct value and format", async () => {
+        const listItem = await screen.findByRole("listitem");
+        const valueWrapper = listItem
+          .closest("li")
+          ?.querySelector(".MuiListItemSecondaryAction-root span");
+
+        expect(valueWrapper).toBeInTheDocument();
+        expect(valueWrapper).toBeVisible();
         expect(valueWrapper?.textContent).toBe(
           formatRawValue(rawValue, "euro")
         );
@@ -49,9 +122,27 @@ describe("<SummaryListItem.tsx />", () => {
           rawValue.toString()
         );
       });
+      test("should render correctly with raw value 0", async () => {
+        cleanup();
+        const testDataValues: SummaryListItemProps = {
+          description: "distance list item",
+          rawValue: 0,
+          formatType: "euro",
+        };
+        const { rawValue } = testDataValues;
+        renderWithTheme(<SummaryListItem {...testDataValues} />, theme);
+        const listItem = await screen.findByRole("listitem");
+        const valueWrapper = listItem
+          .closest("li")
+          ?.querySelector(".MuiListItemSecondaryAction-root span");
+        expect(valueWrapper?.textContent).toBe(
+          formatRawValue(rawValue, "euro")
+        );
+        expect(valueWrapper).toHaveAttribute(
+          "data-raw-value",
+          rawValue.toString()
+        );
+      });
     });
-  });
-  afterEach(() => {
-    cleanup();
   });
 });
