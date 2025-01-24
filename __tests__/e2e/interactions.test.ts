1bef7f955f3d7e5123742578387467e2ff404c20
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-24 10:54:15 +0200

    Add test cases for cartValue input

--- __tests__/e2e/interactions.test.ts
+++ __tests__/e2e/interactions.test.ts
@@ -1,5 +1,6 @@
 import { test, expect, Locator } from "@playwright/test";
 import TestHelper from "./TestHelper";
+import { InputDataTestId } from "../../src/utils/types";
 const { describe, beforeEach } = test;
 
 describe("user interaction", () => {
@@ -10,31 +11,55 @@ describe("user interaction", () => {
   });
 
   describe("inputs", () => {
-    describe("venueSlug input", () => {
-      let venueSlug: Locator;
+    describe("Venue slug", () => {
+      let venueSlugInput: Locator;
+
       beforeEach(() => {
-        venueSlug = testHelper.getInput("venueSlug");
+        venueSlugInput = testHelper.getInput("venueSlug");
       });
-      test("venue slug accepts text", async () => {
+
+      test("accepts text", async () => {
         await testHelper.fillInput("venueSlug", "test 123");
-        await expect(venueSlug).toHaveValue("test 123");
+        await expect(venueSlugInput).toHaveValue("test 123");
       });
       test("input trims white space from start", async () => {
         const testValue = " test 123";
         await testHelper.fillInput("venueSlug", testValue);
-        expect(venueSlug).toHaveValue(testValue.trim());
+        expect(venueSlugInput).toHaveValue(testValue.trim());
       });
       test("input trims white space from end", async () => {
         const testValue = "test 123 ";
         await testHelper.fillInput("venueSlug", testValue);
-        expect(venueSlug).toHaveValue(testValue.trim());
+        expect(venueSlugInput).toHaveValue(testValue.trim());
       });
       test("should have error class if input is empty", async () => {
-        await venueSlug.clear();
-        const textFieldRoot = venueSlug.locator(".."); // parent div of <Textfield />
+        await venueSlugInput.clear();
+        const textFieldRoot = venueSlugInput.locator(".."); // parent div of <Textfield />
         // https://mui.com/material-ui/api/outlined-input/#classes
         await expect(textFieldRoot).toHaveClass(/Mui-error/);
       });
+    });
+  });
+  describe("Cart value", () => {
+    let cartValueInput: Locator;
+    const cartValueTestId: Partial<InputDataTestId> = "cartValue";
+
+    beforeEach(() => {
+      cartValueInput = testHelper.getInput(cartValueTestId);
+    });
+
+    test("should accept integer value", async () => {
+      await testHelper.fillInput(cartValueTestId, "123");
+      expect(cartValueInput).toHaveValue("123");
+    });
+    test("should accept float value", async () => {
+      await testHelper.fillInput(cartValueTestId, "1.23");
+      expect(cartValueInput).toHaveValue("1.23");
+    });
+    test("should have error class if input is empty", async () => {
+      await cartValueInput.clear();
+      const textFieldRoot = cartValueInput.locator("..");
+      await expect(textFieldRoot).toHaveClass(/Mui-error/);
     });
   });
 });
