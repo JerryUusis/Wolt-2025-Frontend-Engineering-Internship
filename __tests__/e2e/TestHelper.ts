8921b4902abdb94a8ffee43b1b7c666ee2b43280
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-25 13:12:43 +0200

    Remove commented code

--- __tests__/e2e/TestHelper.ts
+++ __tests__/e2e/TestHelper.ts
@@ -62,28 +62,6 @@ class TestHelper {
 
     await this.pressCalculateButton()
   }
-  //   const calculateButton = this.page.getByRole("button", {
-  //     name: "Calculate delivery fee",
-  //   });
-  //   const staticPromise = this.page.waitForResponse(
-  //     (response) =>
-  //       response.url() ===
-  //         "https://consumer-api.development.dev.woltapi.com/home-assignment-api/v1/venues/home-assignment-venue-helsinki/static" &&
-  //       response.status() === 200 &&
-  //       response.request().method() === "GET"
-  //   );
-
-  //   const dynamicPromise = this.page.waitForResponse(
-  //     (response) =>
-  //       response.url() ===
-  //         "https://consumer-api.development.dev.woltapi.com/home-assignment-api/v1/venues/home-assignment-venue-helsinki/dynamic" &&
-  //       response.status() === 200 &&
-  //       response.request().method() === "GET"
-  //   );
-
-  //   await calculateButton.click();
-  //   Promise.all([staticPromise, dynamicPromise]);
-  // }
 }
 
 export default TestHelper;
